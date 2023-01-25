c7f3af06b914c10c660c874c6c3b0044da7e6ef5
Victor Jerez <vjerezm@miumg.edu.gt>
2023-01-24 22:57:54 -0600

    Se agregan cambios

--- PromedioNotas/PromedioNotas/src/main/java/PromedioNotas/Promedio.java
+++ PromedioNotas/PromedioNotas/src/main/java/PromedioNotas/Promedio.java
@@ -21,21 +21,17 @@ public class Promedio {
         Scanner teclado= new Scanner(System.in);
 
         int [] prome;
-        prome= new int[20];
+        prome = new int[20];
 
         int i;
-        float total=0;
-        for(i=0;i<20;++i)
+        float total = 0;
+        for(i = 0;i<20;++i)
         {
-            System.out.print("ingrese calificacion del alumno "+i+": ");
-            prome[i]=teclado.nextInt();
-           total=total+prome[i];
+            System.out.print("Ingrese la calificacion del alumno "+i+": ");
+            prome[i] = teclado.nextInt();
+           total = total + prome[i];
         }
-        System.out.println("total es: "+total);
         total= total/20;
-        System.out.println("promedio general es: "+total);
-        for(i=19;i>=0;--i)
-            System.out.println("alumno "+i+" su promedio es: "+prome[i]);  
-    }        
-        
+        System.out.println("El promedio general es de: "+total);  
+    }   //Se borra codigo que sobrecarcaba la vista y mostraba información innecesaria.   
 }
